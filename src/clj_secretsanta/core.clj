fab6cf1198d5321ec7b4122977e1d8f7f208f300
Sascha Koch <sascha.koch@web.de>
2018-11-09 21:30:34 +0100

    some development

--- src/clj_secretsanta/core.clj
+++ src/clj_secretsanta/core.clj
@@ -4,3 +4,34 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def people [
+	{:vorname "Luke" :nachname "Skywalker"}
+	{:vorname "Leia" :nachname "Skywalker"}
+	{:vorname "Toula" :nachname "Portokalos"}
+	{:vorname "Gus" :nachname "Portokalos"}
+	])
+
+
+(defn permutations [c]
+	(lazy-seq
+		(if (seq (rest c))
+			(apply concat (for [x c]
+				(map #(cons x %) (permutations (remove #{x} c)))))
+			[c])))
+
+(defn to-tuples [s]
+	(let [c (vec (conj s (first s)))]
+		(loop [[a & as] c akk []]
+			(if (empty? as)
+				akk
+				(recur as (conj akk [a (first as)]))))))
+
+(defn valid? [c] 
+	(println "valid? c is " c)
+	(loop [[[t1 t2] as t & ts] c b true]
+		(println " t is " t)
+		(println "t1 is " t1 " t2 is " t2)
+		(if (nil? t) 
+			b
+			(recur ts (and b (not (= (:nachname t1) (:nachname t2))))))))
