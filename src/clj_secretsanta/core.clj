e1b37b7a1538b30e5eef8ee46588814057efcda3
Sascha Koch <sascha.koch@web.de>
2018-11-10 00:02:24 +0100

    example moved to test case

--- src/clj_secretsanta/core.clj
+++ src/clj_secretsanta/core.clj
@@ -1,21 +1,5 @@
 (ns clj-secretsanta.core)
 
-
-(def people [
-	{:vorname "Luke" :nachname "Skywalker"}
-	{:vorname "Leia" :nachname "Skywalker"}
-	{:vorname "Toula" :nachname "Portokalos"}
-	{:vorname "Gus" :nachname "Portokalos"}
-	{:vorname "Bruce" :nachname "Wayne"}
-	{:vorname "Virgil" :nachname "Brigman"}
-	{:vorname "Lindsey" :nachname "Brigman"}
-	{:vorname "John" :nachname "Doe"}
-	{:vorname "Jane" :nachname "Doe"}
-	{:vorname "Bobba" :nachname "Fett"}
-	{:vorname "Anakin" :nachname "Skywalker"}	
-	])
-
-
 (defn permutations [c]
 	(lazy-seq
 		(if (seq (rest c))
@@ -29,23 +13,5 @@
 			(recur ts (and b (not (= (:nachname (first t)) (:nachname (second t)))))))))
 
 (defn secret-santa [c]
-	(first (filter valid? (map #(let [fst (first %) cs (conj (vec %) fst)] (partition 2 1 cs)) (permutations c)))))
+	(first (filter valid? (map #(partition 2 1 (take (inc (count %)) (cycle %))) (permutations c)))))
 
-
-
-(defn to-string [c]
-	(loop [[t & ts] c akk (str (:vorname (first t)) " " (:nachname (first t)) )]
-		(if (nil? t)
-			akk
-			(recur ts (str akk " -> " (:vorname (second t)) " " (:nachname (second t)))))
-		))
-
-(defn to-tuples [s]
-       (let [fst (first s) c (conj (vec s) fst)]
-               (loop [[a & as] c akk []]
-                       (if (nil? as)
-                               akk
-                               (recur as (conj akk [a (first as)]))))))
-
-(defn secret-santa-2 [c]
-	(first (filter valid? (map to-tuples (permutations c)))))
--- test/clj_secretsanta/core_test.clj
+++ test/clj_secretsanta/core_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [clj-secretsanta.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def req-people [
+	{:vorname "Luke" :nachname "Skywalker"}
+	{:vorname "Leia" :nachname "Skywalker"}
+	{:vorname "Toula" :nachname "Portokalos"}
+	{:vorname "Gus" :nachname "Portokalos"}
+	{:vorname "Bruce" :nachname "Wayne"}
+	{:vorname "Virgil" :nachname "Brigman"}
+	{:vorname "Lindsey" :nachname "Brigman"}
+	{:vorname "John" :nachname "Doe"}
+	{:vorname "Jane" :nachname "Doe"}
+	{:vorname "Bobba" :nachname "Fett"}
+	{:vorname "Anakin" :nachname "Skywalker"}])
+
+(def res-santa (list
+	'({:vorname "Luke", :nachname "Skywalker"} {:vorname "Toula", :nachname "Portokalos"})
+	'({:vorname "Toula", :nachname "Portokalos"} {:vorname "Leia", :nachname "Skywalker"})
+	'({:vorname "Leia", :nachname "Skywalker"} {:vorname "Gus", :nachname "Portokalos"}) 
+	'({:vorname "Gus", :nachname "Portokalos"} {:vorname "Bruce", :nachname "Wayne"}) 
+	'({:vorname "Bruce", :nachname "Wayne"} {:vorname "Virgil", :nachname "Brigman"}) 
+	'({:vorname "Virgil", :nachname "Brigman"} {:vorname "John", :nachname "Doe"}) 
+	'({:vorname "John", :nachname "Doe"} {:vorname "Lindsey", :nachname "Brigman"}) 
+	'({:vorname "Lindsey", :nachname "Brigman"} {:vorname "Jane", :nachname "Doe"}) 
+	'({:vorname "Jane", :nachname "Doe"} {:vorname "Anakin", :nachname "Skywalker"}) 
+	'({:vorname "Anakin", :nachname "Skywalker"} {:vorname "Bobba", :nachname "Fett"}) 
+	'({:vorname "Bobba", :nachname "Fett"} {:vorname "Luke", :nachname "Skywalker"})))
+
+(deftest secret-santa-test
+  (testing "secret-santa with "
+    (is (= res-santa (secret-santa req-people)))))
